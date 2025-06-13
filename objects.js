08febcfc93f2ec7224ae28c1727bf3e6a126c1d4
DarrylClay2005 <dclay2581@gmail.com>
2025-06-12 19:52:30 -0500

    Update person object with property access and modification examples

--- objects.js
+++ objects.js
@@ -3,3 +3,12 @@ const person = {
     age: 25,
     isStudent: true
 };
+// Access properties
+console.log(person.name); // Dot notation
+console.log(person["age"]); // Bracket notation
+
+// Modify properties
+person.isStudent = false; // Dot notation
+person["name"] = "Bob"; // Bracket notation
+
+console.log(person);
